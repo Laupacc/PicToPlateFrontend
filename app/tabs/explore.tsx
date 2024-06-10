73fd6a9567875c28de63c3ea19b6f929ef1b0a22
Your name <Your email>
2024-06-10 17:45:48 +0200

    lots

---
+++ app/tabs/explore.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+export default function Explore() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>ExplorePage</Text>
+    </View>
+  );
+}
---
+++ app/tabs/index.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>HomePage</Text>
+    </View>
+  );
+}
