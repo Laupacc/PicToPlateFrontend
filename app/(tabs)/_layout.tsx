aed907a0f219a02a184e6502036fd4c00f44744c
Your name <Your email>
2024-07-03 01:02:36 +0200

    tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,16 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { useColorScheme } from "react-native";
-import { TabBarIcon } from "@/components/TabBarIcon";
+import {
+  Platform,
+  useColorScheme,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { Colors } from "@/constants/Colors";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -21,87 +29,114 @@ export default function TabLayout() {
           marginBottom: -3,
           marginTop: 3,
         },
-        // tabBarStyle: {
-        //   borderTopLeftRadius: 30,
-        //   borderTopRightRadius: 30,
-        //   backgroundColor: Colors[colorScheme ?? "light"].background,
-        //   padding: 5,
-        //   height: 70,
-        //   position: "absolute",
-        //   bottom: 0,
-        //   left: 0,
-        //   right: 0,
-        //   overflow: "hidden",
-        //   elevation: 0,
-        // },
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 25,
+          left: 20,
+          right: 20,
+          borderRadius: 20,
+          backgroundColor: "#e2e8f0",
+          ...styles.shadow,
+        },
+        tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
         name="search"
         options={{
           title: "Search",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "restaurant" : "restaurant-outline"}
-              color={color}
-              iconSet="Ionicons"
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <View>
+                <Ionicons
+                  name={focused ? "restaurant" : "restaurant-outline"}
+                  color={color}
+                  size={28}
+                />
+              </View>
+            );
+          },
         }}
       />
+
       <Tabs.Screen
         name="favourites"
         options={{
           title: "Favourites",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "heart" : "heart-outline"}
-              color={color}
-              iconSet="MaterialCommunityIcons"
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <View>
+                <MaterialCommunityIcons
+                  name={focused ? "heart" : "heart-outline"}
+                  color={color}
+                  size={28}
+                />
+              </View>
+            );
+          },
         }}
       />
       <Tabs.Screen
         name="camera"
         options={{
           title: "Camera",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={"camera-retro"}
-              color={color}
-              iconSet="FontAwesome"
-              size={40}
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <View
+                style={{
+                  top: Platform.OS === "ios" ? -10 : -20,
+                  width: Platform.OS === "ios" ? 50 : 60,
+                  height: Platform.OS === "ios" ? 50 : 60,
+                  borderRadius: Platform.OS === "ios" ? 25 : 30,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: Colors[colorScheme ?? "light"].tint,
+                  ...styles.shadow,
+                }}
+              >
+                <FontAwesome
+                  name={focused ? "camera-retro" : "camera-retro"}
+                  color={Colors[colorScheme ?? "light"].background}
+                  size={40}
+                />
+              </View>
+            );
+          },
         }}
       />
+
       <Tabs.Screen
         name="fridge"
         options={{
           title: "Fridge",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "fridge" : "fridge-outline"}
-              color={color}
-              iconSet="MaterialCommunityIcons"
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <View>
+                <MaterialCommunityIcons
+                  name={focused ? "fridge" : "fridge-outline"}
+                  color={color}
+                  size={28}
+                />
+              </View>
+            );
+          },
         }}
       />
+
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={"user-secret"}
-              color={color}
-              iconSet="FontAwesome"
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <View>
+                <FontAwesome name={"user-secret"} color={color} size={28} />
+              </View>
+            );
+          },
         }}
       />
+
       <Tabs.Screen
         name="recipeCard"
         options={{
@@ -126,3 +161,16 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
